55b14c9c4ec200e8ecb972a4bc9d18a4697d55ec
Abhishek333k <59314939+Abhishek333k@users.noreply.github.com>
2026-03-16 09:44:05 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -131,37 +131,39 @@ function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
     let projectsHTML = "";
     projects.forEach(project => {
+        
+        // Architect Logic: Lock the code button if private
+        let codeButton = project.links.code === "private" 
+            ? `<span class="btn" style="background: #f1f3f4; color: #5f6368; border: 1px solid #dadce0; cursor: not-allowed; box-shadow: none;"><i class="fas fa-lock"></i> Proprietary</span>`
+            : `<a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>`;
+
+        // Architect Logic: Change 'View Live' to 'View Gallery' if it's the POS system
+        let viewButtonText = project.links.view.includes("openGallery")
+            ? `<i class="fas fa-images"></i> View Gallery`
+            : `<i class="fas fa-eye"></i> View Live`;
+
         projectsHTML += `
         <div class="grid-item ${project.category}">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="../assets/images/projects/${project.image}.png" alt="project" />
-      <div class="content">
-        <div class="tag">
-        <h3>${project.name}</h3>
-        </div>
-        <div class="desc">
-          <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>`
+            <div class="box tilt" style="width: 380px; margin: 1rem">
+                <img draggable="false" src="../assets/images/projects/${project.image}.png" alt="project" />
+                <div class="content">
+                    <div class="tag"><h3>${project.name}</h3></div>
+                    <div class="desc">
+                        <p>${project.desc}</p>
+                        <div class="btns">
+                            <a href="${project.links.view}" class="btn" target="_blank">${viewButtonText}</a>
+                            ${codeButton}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
     });
     projectsContainer.innerHTML = projectsHTML;
 
-    // isotope filter products
     var $grid = $('.box-container').isotope({
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows',
-        masonry: {
-            columnWidth: 200
-        }
+        itemSelector: '.grid-item', layoutMode: 'fitRows', masonry: { columnWidth: 200 }
     });
-
-    // filter items on button click
     $('.button-group').on('click', 'button', function () {
         $('.button-group').find('.is-checked').removeClass('is-checked');
         $(this).addClass('is-checked');
@@ -169,7 +171,6 @@ function showProjects(projects) {
         $grid.isotope({ filter: filterValue });
     });
 }
-
 getProjects().then(data => {
     showProjects(data);
 });
